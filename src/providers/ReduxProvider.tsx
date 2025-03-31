de01703c6c38260c1a5c7fd0c138c6bd235f85d1
Joshua Magani <maganijoshua25@gmail.com>
2025-03-31 21:44:00 +0100

    events api

---
+++ src/providers/ReduxProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { persistor, store } from "@/redux/store";
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+const ReduxProvider = ({ children }: { children: ReactNode }) => {
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				{children}
+			</PersistGate>
+		</Provider>
+	);
+};
+
+export default ReduxProvider;
